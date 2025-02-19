68b6a13700decd4825dbffb57dc17d2965aaa489
JuanManuelGomezOchoa <utm23090605@utma.edu.mx>
2025-02-18 19:03:12 -0600

    Controller de pokemon y favorite_pokemon

--- app/controllers/pokemon_favorite_controller.py
+++ app/controllers/pokemon_favorite_controller.py
@@ -1,4 +1,29 @@
-#Crea
-#Elimina
-#Get all
-#Modificar la clase del modelo y evitar que se usen metodos indebidos
+
+from flask import Blueprint, request, jsonify 
+from app.schemas.pokemon_favorite_schema import PokemonFavoriteSchema
+from app.models.factory import ModelFactory
+from marshmallow import ValidationError
+from bson import ObjectId
+bp = Blueprint("pokemon_favorites", __name__, url_prefix="/pokemon_favorites")
+pokemon_favorite_schema = PokemonFavoriteSchema()
+pokemon_favorite_model = ModelFactory.getModel("pokemon_favorites")
+@bp.route("/register_pokemon", methods=["POST"])
+def register():
+    try:
+        data = pokemon_favorite_schema.load(request.json)
+        pokemon_id= pokemon_favorite_model.create(data)
+        return jsonify({"pokemon_id":str(pokemon_id)}, 200)
+    
+    except ValidationError as err:
+        return jsonify("Los parametros enviados son correctos", 400)
+    
+    
+@bp.route("/delete_favorite_pokemon/<string:pokemon_id>", methods=["DELETE"])
+def delete(pokemon_id):
+    pokemon_favorite_model.delete(ObjectId(pokemon_id))
+    return jsonify("Pokemon eliminado con exito", 200)
+
+@bp.route("/get_pokemons/", methods=["GET"])
+def get_pokemon():
+    pokemon = pokemon_favorite_model.find_all()
+    return jsonify(pokemon, 200)
