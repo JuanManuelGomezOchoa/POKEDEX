752e55a776035c1908dbc78634fae1b4132d9d31
JuanManuelGomezOchoa <utm23090605@utma.edu.mx>
2025-04-06 13:08:48 -0600

    Maquetado y vistas de react-native de Login, RegisterUser, RecoverPassword y UpdateUser

--- app/App.js
+++ app/App.js
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import login from './Login';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import RecoverPassword from './RecoverPassword';
+import RegisterUser from './RegisterUser';
+
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="login">
+        <Stack.Screen name="login" component={login} />
+        <Stack.Screen name='RecoverPassword' component={RecoverPassword}/>
+        <Stack.Screen name="RegisterUser" component={RegisterUser} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ app/RecoverPassword.js
@@ -0,0 +1,76 @@
+import { StatusBar } from 'expo-status-bar';
+import { useNavigation } from '@react-navigation/native';
+import { StyleSheet, Text, View, Image, TextInput, Pressable} from 'react-native';
+
+export default function RecoverPassword() {
+    const navigation = useNavigation()
+  return (
+    <View style={styles.container}>
+      <View> {/*Container-image*/}
+        <Image source={{uri:"https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Pok%C3%A9_Ball_icon.svg/2052px-Pok%C3%A9_Ball_icon.svg.png"}}
+        width={200}
+        height={200}/>
+      </View>
+      <View> {/*Contaiiner-form*/}
+        <Text style={styles.title}>Recuperar contraseña</Text>{/*title*/}
+        <Text style={styles.label}>Correo Electronico:</Text>{/*label*/}
+        <TextInput style={styles.input}/>
+        <Pressable style={styles.send}>
+          <Text style={styles.send.textButton}>Recuperar</Text>
+        </Pressable>
+      </View>
+
+      <View style={styles.containerFooter}>
+        <Text style={styles.containerFooter.texts}>Iniciar sesion</Text>
+        <Text onPress={()=> navigation.navigate("RegisterUser")} style={styles.containerFooter.texts}>Registrate</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex:1,
+    backgroundColor: '#fff',
+    padding:10,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize:30,
+    fontWeight:"bold"
+  },
+  label: {
+    fontSize:20,
+    fontWeight:"bold"
+  },
+  input: {
+    borderRadius:10,
+    borderColor:"black",
+    borderWidth:2,
+    fontSize:20,
+    width:"auto"
+  },
+  send: {
+    backgroundColor:"red",
+    width:"auto",
+    height:"auto",
+    borderRadius:10,
+    marginTop:15,
+    alignItems:"center",
+    textButton:{
+      color:"white",
+      fontSize:30,
+      fontWeight:"bold"
+    }
+  },
+  containerFooter: {
+    justifyContent:"space-between",
+    alignItems:"center",
+    texts:{
+      fontSize:20,
+      margin:5,
+      color:"blue"
+    }
+  }
+});
